07099c5fb38b0a76ff8b4ac67958fe91eb66d913
Marcio Zebedeu <marciozebedeu@gmail.com>
2021-10-06 13:11:50 +0100

    Add files via upload

---
+++ home-template.php
@@ -0,0 +1,184 @@
+<?php
+
+
+/* 
+Template Name: Home 2
+*/
+
+get_header();
+?>
+
+<section class="main_banner">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<div class="banner-text">
+<h1>Angola's Trading Platform for Farmers</h1>
+<p>Kepya is a real time and transparent marketplace engine to tackle some of the biggest agri-business problems in Angola.</p>
+
+<a href="#"><button class="main_btn">Create Free Account</button></a>
+
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 kp_videoBox">
+<div class="kp_videoSet"><video controls="controls" width="300" height="150">
+<source src="http://192.168.1.14/wordpress/kepya/wp-content/themes/kepya/images/Kepya.mp4" type="video/mp4" />
+<source src="http://192.168.1.14/wordpress/kepya/wp-content/themes/kepya/images/Kepya.mp4" type="video/ogg" />
+</video>
+<div class="mg_videoSetFooter"></div>
+</div>
+</div>
+</div>
+</div>
+</section><section class="features">
+<div class="container">
+
+	<div class="card">
+			<?php
+        		// Start the loop.
+        		while ( have_posts() ) :
+        			the_post();
+
+        			// Include the single post content template.
+        			
+        			the_content();
+
+        			
+        			// End of the loop.
+        		endwhile;
+        		?>
+	</div>
+
+<div class="">
+<div class="row">
+<div class="col-md-12 data-section">
+<h2>Produce Business in Angola and Enable more business open doors in agri eco-framework through coordinated inventory network</h2>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12 data-section">
+<p class="product-p">Community oriented stage intended for the Integration of Agri business organize and innovation for utilizing Producers , Aggregators ,Transporters , Compradors and Packaging to convey crisp produce in auspicious way.</p>
+
+</div>
+</div>
+<div class="row">
+<div class="col-md-6 category">
+<div id="accordion">
+<ul>
+ 	<li><a class="home_accordian home-tab disabled" href="#">Producers</a><i>+</i>They are responsible for ensuring production through family and commercial, manual or mechanized farming.</li>
+ 	<li><a class="home_accordian2 home-tab disabled" href="#">Aggregators</a><i>+</i>They are young people living in rural areas and more easily using mobile technology. They become responsible for coordinating, aggregating and representing producer groups within the platform.</li>
+ 	<li><a class="home_accordian3 home-tab disabled" href="#">Compradors</a><i>+</i>They represent trade units distributed throughout the country. They are responsible for ensuring the supply of agricultural products to the consumer market.</li>
+ 	<li><a class="home_accordian4 home-tab disabled" href="#">Transporters </a><i>+</i>When a transaction is successful, registered carriers on the platform are notified of the freight opportunity and therefore have a deadline to accept the freight and schedule the collection and delivery of the goods.</li>
+</ul>
+</div>
+<a href="#"><button class="main_btn">Explore Marketplace</button></a>
+
+</div>
+<div class="col-md-6"><img class="right_img" src="http://localhost/wordpress/wp-content/uploads/2019/11/right.png" /></div>
+</div>
+</div>
+</div>
+</section><section class="whychoose">
+<div class="container">
+<div class="row analytics">
+<div class="col-md-6"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/anaylitics.png" /></div>
+<div class="col-md-6">
+<h2>Enormous Data Analytics to lessen value wasteful aspects in Agri Trade</h2>
+We discover, interpret, and communicate meaningful patterns in Agri trade across <a href="#">Angola</a>.
+
+<a href="#"><button class="main_btn">Explore Analytics</button></a>
+
+</div>
+</div>
+</div>
+</section><section class="features">
+<div class="container">
+<div class="row analytics">
+<div class="col-md-6">
+<h2>Cost Analyzer</h2>
+An all year arranging device for your Farm to know where you remain anytime of the period.
+
+<a href="#"><button class="main_btn">Get Started Here</button></a>
+
+</div>
+<div class="col-md-6"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/cost.png" /></div>
+</div>
+</div>
+</section><section class="whychoose">
+<div class="container">
+<div class="row analytics">
+<div class="col-md-6"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/search.png" /></div>
+<div class="col-md-6">
+<h2>Search and Match Engine</h2>
+Indexes and organizes data inserted by entities and generated in trading
+
+<a href="#"><button class="main_btn">Explore Analytics</button></a>
+
+</div>
+</div>
+</div>
+</section><section class="features">
+<div class="container">
+<div class="data-section">
+<h2>Why Choose Us</h2>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+<div class="why-choose">
+<div class="why-choose-left">
+<h3><img src="http://localhost/wordpress/wp-content/uploads/2019/11/why-choose-1.png" /> Online Agri Alliance</h3>
+Contract farming arrangements of different types will be made available to farmers for both subsistence and commercial crops.
+<h3><img src="http://localhost/wordpress/wp-content/uploads/2019/11/why-choose-1.png" /> Trade Partnership</h3>
+Build a strong farming &amp; trading network to support each other with solid integration of logistics and packaging industry.
+
+</div>
+<div class="choose-img"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/why-choose.png" /></div>
+<div class="why-choose-right">
+<h3><img src="http://localhost/wordpress/wp-content/uploads/2019/11/why-choose-3.png" /> Better Pricing Options</h3>
+Offering cashless solution for all parties in the produce eco system.
+<h3><img src="http://localhost/wordpress/wp-content/uploads/2019/11/why-choose-4.png" /> Exclusive Data Analytics</h3>
+Suspendisse ultricies nisi vel quam suscipit, et rutrum odit porttitor.
+
+</div>
+</div>
+</div>
+</div>
+</section><section class="app_banner lazybgnone lazybg">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 col-lg-offset-6 col-md-offset-6 analytics2">
+<h2>Don't miss out!
+Start your free trial today.</h2>
+<a href="#" target="_blank" rel="noopener noreferrer"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/apple.jpg" /></a>
+<a href="#" target="_blank" rel="noopener noreferrer"><img src="http://localhost/wordpress/wp-content/uploads/2019/11/playstore.png" /></a>
+
+</div>
+</div>
+</div>
+</section><section class="latest_news">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="data-section">
+<h2 class="news">Latest Update</h2>
+</div>
+</div>
+</div>
+<div class="row latest-blog">[kp-page-blogs]</div>
+</div>
+<div>
+<div class="">
+<div class="row"></div>
+</div>
+<div class="">
+<div class="row">
+<div class="col-md-12 data-section">
+<p class="product-p"></p>
+
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<?php get_footer(  ); 
---
+++ template-parts/content-home.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template part for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+
+<div>    
+	<?php
+		/* translators: %s: Name of current post */
+		the_content( sprintf(
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+			get_the_title()
+		) );
+
+		wp_link_pages( array(
+			'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+			'after'       => '</div>',
+			'link_before' => '<span>',
+			'link_after'  => '</span>',
+			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+			'separator'   => '<span class="screen-reader-text">, </span>',
+		) );
+	?>
+</div><!-- .entry-content -->
